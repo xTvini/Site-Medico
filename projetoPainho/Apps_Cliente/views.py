cbfa5e8f2f82f4905d569af26cf3f7922a067290
xTvini <142419660+xTvini@users.noreply.github.com>
2025-02-12 03:59:55 -0300

    .

--- projetoPainho/Apps_Cliente/views.py
+++ projetoPainho/Apps_Cliente/views.py
@@ -24,25 +24,20 @@ def PAGINAINICIALCLIENTE(request):
 def marcarConsulta(request):
     selected_estado = request.GET.get('estado', None)
     selected_especialidade = request.GET.get('especialidade', None)
-
     # Filtra os hospitais que possuem a especialidade e estão no estado correto
     hospitais = Hospital.objects.filter(estado=selected_estado, especialidade=selected_especialidade)
-
     return render(request, "Clientes/marcarConsulta.html", {'estado': selected_estado,'especialidade': selected_especialidade,'hospitais': hospitais})
 
 @login_required
 def calendario(request):
     consultas = Consulta.objects.all()
-
-    eventos = [
-        {
-            "titulo": consulta.titulo,
-            "descricao": f"Médico: {consulta.medico.firstname} ({consulta.medico.crm} - {consulta.medico.estado})",
-            "data": consulta.data.strftime("%Y-%m-%d"),
-            "horario": consulta.horario.strftime("%H:%M") if consulta.horario else "",
-            "hospital": consulta.hospital.nome,
-        }
-        for consulta in consultas
-    ]
+    hospitais = Hospital.objects.all()
+    medicos = Doctor.objects.all()
     dias_da_semana = ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SAB']
-    return render(request, "Clientes/calendario.html", {"eventos_json": json.dumps(eventos), "dias_da_semana": dias_da_semana})
+
+    return render(request, "Clientes/calendario.html", {
+        "consultas": consultas,
+        "hospitais": hospitais,
+        "medicos": medicos,
+        "dias_da_semana": dias_da_semana
+    })
